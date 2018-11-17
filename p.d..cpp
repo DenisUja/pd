b8ea10a1aea1fc5a008831bdecef735a8d9876c8
DenisUja <44978523+DenisUja@users.noreply.github.com>
2018-11-17 12:03:13 +0200

    Add files via upload

---
+++ p.d..cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+   int i, n, a[100];
+   cout<<"n=";cin>>n;
+   cout<<"Introduceti numerele:"<<endl;
+   for (i=0;i<n;i++)
+      {
+      cout<<"a["<<i<<"]=";cin>>a[i];
+      }
+}
